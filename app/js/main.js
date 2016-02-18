ba7ea7d80bda7c290edc5ef2c9106c85090d2868
Xko97 <feelbots@gmail.com>
2016-02-18 19:38:33 +0300

    edit stucture application

---
+++ app/js/main.js
@@ -0,0 +1,69 @@
+
+(function(){
+    'use strict';
+    
+    var instagramModule = angular.module('instagramModule',['ngRoute']);
+
+
+instagramModule.service('Url',function(){
+    var access_token, url, str = window.location.hash;
+
+    if(str) {
+        url = str.substr(1);
+        access_token = url.split('=')[1];
+    }
+
+    this.access_token = access_token;
+});
+
+
+instagramModule.controller('UserCtrl',["$scope","$http","Url",function($scope,$http,Url){
+
+    var token = Url.access_token;
+    $scope.token = token;
+
+   $http.jsonp("https://api.instagram.com/v1/users/self/?callback=JSON_CALLBACK&access_token=" + token).success(function(response){
+       $scope.me = response.data;
+   });
+
+    $http.jsonp("https://api.instagram.com/v1/users/self/media/recent/?callback=JSON_CALLBACK&access_token=" + token).success(function(response){
+        $scope.photos = response.data;
+        //console.log($scope.photos);
+    });
+
+}]);
+
+
+instagramModule.controller('SearchCtrl',['$scope','$http','Url',function($scope,$http,Url){
+    var token = Url.access_token;
+
+    $scope.searchUser = function(arg){
+        $http.jsonp('https://api.instagram.com/v1/tags/' + arg + '/media/recent?callback=JSON_CALLBACK&count=100&access_token='+token).success(function(response){
+            $scope.photos = response.data;
+            console.log($scope.photos);
+        });
+    };
+}]);
+
+
+instagramModule.directive('fancybox',function(){
+   return {
+       link:function(scope,element,attrs){
+           $(element).fancybox({
+               openEffect  : 'none',
+               closeEffect : 'none',
+               nextEffect  : 'none',
+               prevEffect  : 'none',
+               padding     : 0,
+               margin      : [20, 60, 20, 60]
+           });
+       }
+   };
+});
+
+
+
+
+    
+})();
+
--- gulpfile.js
+++ gulpfile.js
@@ -6,9 +6,25 @@ var gulp = require('gulp'),
     jade = require('gulp-jade'),
     minify = require('gulp-minify'),
     concat = require('gulp-concat'),
-    rename = require('gulp-rename');
+    rename = require('gulp-rename'),
+    flatten = require('gulp-flatten'),
+    imageop = require('gulp-image-optimization');
 
 
+
+var bower_js = [
+    "app/bower_components/jquery/dist/jquery.js",
+    "app/bower_components/angular/angular.js",
+    "app/bower_components/angular-route/angular-route.js",
+    "app/bower_components/fancybox/source/jquery.fancybox.js",
+]
+
+
+
+var bower_css = bowercatalog('app/bower_components/',[
+    'reset-css/reset.css',
+    'fancybox/source/jquery.fancybox.css'
+]);
 
 
 gulp.task('connect', function() {
@@ -33,11 +49,18 @@ gulp.task('sass',function(){
 	.pipe(sass({
 		outputStyle:"expanded"
 	})).on('error',console.log)
-	.pipe(rename("build.min.css"))
-	.pipe(gulp.dest('./public/css'))
+	.pipe(rename("custom_compile.css"))
+	.pipe(gulp.dest('./public/css/'))
   .pipe(connect.reload());
 });
 
+
+gulp.task('bower_css',function(){
+    gulp.src(bower_css)
+        .pipe(concat('bower_compile.css'))
+        .pipe(minify())
+        .pipe(gulp.dest('public/css/'))
+})
 
 gulp.task('watch',function(){
   gulp.watch('app/jade/*.jade',['jade']);
@@ -45,4 +68,50 @@ gulp.task('watch',function(){
 });
 
 
-gulp.task('default',['jade','sass','watch','connect']);
+gulp.task('bower_javascript',function(){
+
+    
+   gulp.src(bower_js)
+        .pipe(concat('bower_compile.js'))
+        .pipe(minify())
+        .pipe(gulp.dest('public/js/'))
+});
+
+gulp.task('custom_javascript',function(){
+   gulp.src('app/js/*.js')
+        .pipe(concat('custom_compile.js'))
+        .pipe(minify())
+        .pipe(gulp.dest('public/js/'))
+});
+
+gulp.task('images',function(){
+    gulp.src(['app/img/**/*.png','app/img/**/*.jpg','app/img/**/*.gif','app/img/**/*.jpeg']).pipe(imageop({
+        optimizationLevel: 5,
+        progressive: true,
+        interlaced: true
+    })).pipe(gulp.dest('public/img'));
+    
+    console.log('Optimization ready');
+});
+
+
+gulp.task('font',function(){
+    gulp.src('app/fonts/**/*')
+        .pipe(gulp.dest('public/fonts'))
+});
+
+gulp.task('default',['jade','sass','bower_css','font','images','bower_javascript','custom_javascript','watch','connect']);
+
+
+
+
+function bowercatalog(bower,arr){
+    
+    var copy = [];
+    
+    for(var i = 0; i < arr.length; i++){
+        copy.push(bower+arr[i]);
+    }
+    
+    return copy;
+}
